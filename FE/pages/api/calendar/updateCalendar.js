700ee0408bf272c731f71d7118a18370a875b972
sondcniju <132870331+sondcniju@users.noreply.github.com>
2025-03-06 23:24:40 +0700

    Update homepage,Booking,Search

---
+++ FE/pages/api/calendar/updateCalendar.js
@@ -0,0 +1,11 @@
+import api from 'utils/api';
+
+export const updateCalendarEntry = async (calendarId, calendarData) => {
+	try {
+		const response = await api.put(`/Calendar/${calendarId}`, calendarData);
+		return response.data;
+	} catch (error) {
+		console.error('Error updating calendar entry:', error);
+		throw error;
+	}
+};
---
+++ FE/pages/api/city/getCityList.js
@@ -0,0 +1,11 @@
+import api from "@/utils/api";
+
+export const getCityList = async () => {
+	try {
+		const response = await api.get('/HomeStay/get-city-list');
+		return response.data;
+	} catch (error) {
+		console.error('Error fetching city list:', error);
+		throw error;
+	}
+};
