a54286c9e2c634db04467cda77ed63e942bea8fc
sondcniju <132870331+sondcniju@users.noreply.github.com>
2025-03-07 21:20:59 +0700

    file thiếu

--- FE/pages/api/calendar/deleteCalendar.js
+++ FE/pages/api/calendar/deleteCalendar.js
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
-import api from '@/utils/api';
-=======
 import api from "@/utils/api";
->>>>>>> Stashed changes
 
 export const deleteCalendarEntry = async (calendarId) => {
 	try {
