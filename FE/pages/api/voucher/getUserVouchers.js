819fdc31c3fa0e7609c1419bf3921a0d19044cce
sondcniju <132870331+sondcniju@users.noreply.github.com>
2025-03-08 21:23:50 +0700

    Voucher manager và update user voucher

---
+++ FE/pages/api/voucher/getUserVouchers.js
@@ -0,0 +1,11 @@
+import api from '@/utils/api';
+
+export const getUserVouchers = async () => {
+	try {
+		const response = await api.get('/Voucher/user-vouchers');
+		return response.data;
+	} catch (error) {
+		console.error('Error fetching user vouchers:', error);
+		throw error;
+	}
+};
