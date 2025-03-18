3a00fec49d18be461b9ae760be532e82f66b046e
Nghiatt1123 <nhuenguyenphuong@gmail.com>
2025-03-18 19:43:24 +0700

    Test Billing qua mail

--- BE/RUNAHMS-main/Authentication/RUNAHMS-API/Controllers/BookingController.cs
+++ BE/RUNAHMS-main/Authentication/RUNAHMS-API/Controllers/BookingController.cs
@@ -418,6 +418,117 @@ namespace API.Controllers
             return Ok(response);
         }
 
+        public static String SendMailBooking(HomeStay homeStay, Booking booking)
+        {
+
+            return
+            $@"
+            <!DOCTYPE html>
+            <html lang=""en"">
+   <html lang=""en"">
+            <head>
+                <meta charset=""UTF-8"">
+                <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
+                <title>Booking Confirmation</title>
+                <style>
+                    body {{
+                        font-family: Arial, sans-serif;
+                        background-color: #f9f9f9;
+                        margin: 0;
+                        padding: 0;
+                    }}
+                    .container {{
+                        background-color: #FFFAF0;
+                        padding: 20px;
+                        border-radius: 8px;
+                        box-shadow: 0 0 10px rgba(0,0,0,0.1);
+                        width: 100%;
+                        max-width: 600px;
+                        box-sizing: border-box;
+                        margin: auto;
+                    }}
+                    .hotel-info {{
+                        text-align: center;
+                        margin-bottom: 20px;
+                    }}
+                    .hotel-image {{
+                        width: 100%;
+                        height: auto;
+                        border-radius: 8px;
+                    }}
+                    .room-type {{
+                        font-size: 18px;
+                        font-weight: bold;
+                    }}
+                    .details, .cancellation {{
+                        font-size: 14px;
+                        color: #666;
+                    }}
+                    .cancellation span {{
+                        color: #d9534f;
+                    }}
+                    .form-group {{
+                        margin-bottom: 15px;
+                    }}
+                    .form-group label {{
+                        display: block;
+                        margin-bottom: 5px;
+                    }}
+                    .form-group input, .form-group select, .form-group button {{
+                        width: 100%;
+                        padding: 10px;
+                        border: 1px solid #ccc;
+                        border-radius: 4px;
+                        box-sizing: border-box;
+                    }}
+                    .price-detail {{
+                        text-align: right;
+                        font-size: 14px;
+                        color: #666;
+                    }}
+         
+                    @media (max-width: 768px) {{
+                        .price-detail {{
+                            text-align: left;
+                        }}
+                    }}
+                </style>
+            </head>
+            <body>
+                <div class=""container"">
+                    <div class=""hotel-info"">
+                        <img src=""{homeStay.MainImage}"" alt=""Hotel Image"" class=""hotel-image"">
+                        <h2>{homeStay.Name} - {homeStay.Address}</h2>
+                    </div>
+                    <div class=""form-group"">
+                        <label for=""checkin"">Check-in</label>
+                        <input type=""date"" id=""checkin"" name=""checkin"" value=""{booking.CheckInDate:yyyy-MM-dd}"" disabled readonly required>
+                    </div>
+                   <div class=""form-group"">
+                        <label for=""checkin"">Check-Out</label>
+                        <input type=""date"" id=""checkin"" name=""checkin"" value=""{booking.CheckOutDate:yyyy-MM-dd}"" disabled readonly required>
+                    </div>
+                    <div class=""form-group"">
+                        <label for=""fullname"">Fullname</label>
+                        <input type=""text"" id=""fullname"" name=""fullname"" value=""{booking.User.FullName}"" disabled readonly  required>
+                    </div>
+                    <div class=""form-group"">
+                        <label for=""email"">Email</label>
+                        <input type=""email"" id=""email"" name=""email"" value=""{booking.User.Email}"" disabled readonly required>
+                    </div>
+                    <div class=""form-group"">
+                        <label for=""phone"">Phone</label>  
+                        <input type=""tel"" id=""phone"" name=""phone"" value=""{booking.User.Phone}"" disabled readonly required>
+                    </div>
+                    <div class=""price-detail"">
+                        <p>Check-In-Date: <span>{booking.CheckInDate.ToString("dd/MM/yyy")} $</span></p>
+                        <p>Check-Out-Date: <span>{booking.CheckOutDate.ToString("dd/MM/yyy")} $</span></p>
+                        <p>Total: <span>{booking.TotalPrice} $</span></p>
+                    </div>
+                </div>
+            </body>
+            </html>";
+        }
     }
 }
 
