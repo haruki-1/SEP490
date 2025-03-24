2764275a213b671b46c9604fbeb275d7ed92dd0f
sondcniju <132870331+sondcniju@users.noreply.github.com>
2025-03-24 22:19:47 +0700

    Update feedback, voucher, cancel booking và payement thành công

---
+++ FE/pages/api/booking/cancelBooking.js
@@ -0,0 +1,14 @@
+import api from "@/utils/api";
+
+export const cancelBooking = async (bookingID, reasonCancel) => {
+	try {
+		const response = await api.post('/Booking/cancel', {
+			bookingID,
+			reasonCancel,
+		});
+		return response.data;
+	} catch (error) {
+		console.error('Error canceling booking:', error);
+		throw error;
+	}
+};
