1e716eeff1e6324c02af7222d426c67d181ffa72
Nghiatt1123 <nhuenguyenphuong@gmail.com>
2025-03-24 23:21:16 +0700

    Update BookingControllerTesting

---
+++ BE/RUNAHMS-main/Authentication/APITesting/DTO/HomeStayDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject.DTO;
+
+namespace APITesting.DTO
+{
+     public class HomeStayDTO
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public IEnumerable<CalendarDTO> Calendar { get; set; }
+        public IEnumerable<AddAmenityDTO> Amenities { get; set; }
+        public IEnumerable<AddFacilityDTO> Facility { get; set; }
+    }
+}
---
+++ BE/RUNAHMS-main/Authentication/APITesting/DTO/RevenueStatisticResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APITesting.DTO
+{
+    public class RevenueStatisticResult
+    {
+        public string Month { get; set; }
+        public int Booking { get; set; }
+        public decimal Revenue { get; set; }
+    }
+}
